08284c27042e0324edcc4be54be7f8a458b1e36b
joykk <zzyyshi@qq.com>
2016-10-08 12:07:07 +0800

    start
    1.0

---
+++ getNovels.py
@@ -0,0 +1,54 @@
+# -*- encoding=UTF-8 -*-
+#python2
+import urllib,urllib2,re,random,time
+import MySQLdb
+from bs4 import BeautifulSoup
+
+def get_book_detail_url():
+    index=0
+    while (True):
+        url = 'http://www.23wx.com/class/8_'+str(index+1)+'.html'
+        index += 1
+        try:
+            conn = MySQLdb.connect(user='root', passwd='123456',
+                                   host='localhost', db='langdinovel', charset='utf8')
+            cur = conn.cursor()
+            user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+
+            values = {'name': 'Michael Foord',
+                      'location': 'pythontab',
+                      'language': 'Python'}
+            headers = {'User-Agent': user_agent}
+            data = urllib.urlencode(values)
+            req = urllib2.Request(url, data, headers)
+            html = urllib2.urlopen(req).read()
+            print "working"
+            soup = BeautifulSoup(html, 'html.parser', from_encoding='gbk')
+            #print "soup----------\n", soup
+            list = soup.find_all('tr', bgcolor="#FFFFFF")
+            if(len(list)<=0):
+                print "over"
+                break;
+            for tr in list:
+                tds = tr.findAll('td')
+                novel_name = tds[0].get_text()
+                novel_author = tds[2].get_text()
+                novel_intro_url = tds[0].find('a')['href']
+                novel_catalog_url = tds[1].find('a')['href']
+                sql = "insert into novels(novel_navigation,novel_name,novel_author,novel_catalog_url,novel_intro_url)VALUES ('11',%s,%s,%s,%s)"
+                bol = cur.execute(sql , (novel_name, novel_author, novel_catalog_url, novel_intro_url))
+        except:
+            import traceback
+            traceback.print_exc()
+            cur.close()
+            conn.commit()
+            conn.close()
+            break;
+        cur.close()
+        conn.commit()
+        conn.close()
+        if (bol == 1):
+            print "success url-", url
+        print "sleeping"
+        time.sleep(random.random() * 5)
+get_book_detail_url()
---
+++ get_all_chapter.py
@@ -0,0 +1,91 @@
+# -*- encoding=UTF-8 -*-
+import urllib,urllib2,re,time,random,MySQLdb
+from bs4 import BeautifulSoup
+from header import *
+
+def start_read(novel_name,novel_author):
+    novel_info=get_novel_id_DB(novel_name,novel_author)
+    if(novel_info=='nonono'):
+        print "sorry can't find the novel"
+        return
+    req_header = getHeader()
+    req_timeout = 20
+    req = urllib2.Request(novel_info[1], None, req_header)
+    resp = urllib2.urlopen(req, None, req_timeout)
+    html = resp.read()
+    soup=BeautifulSoup(html,'html.parser', from_encoding='gbk')
+    firstCap=soup.find('table',id="at").find('a')
+    url_temp = novel_info[1] + firstCap['href']
+    index=0
+    while(True):
+        index+=1
+        url_next=get_chapter_text(url_temp,novel_info,index)
+        if((not url_next==None) and '/html/' in url_next):
+            url_next=url_next[len(novel_info[1])-len(host):]
+        if (url_next==None):
+            print 'ok! read sucess'
+            break
+        url_temp = novel_info[1]+url_next
+        print "sleeping ZZZ"
+        time.sleep(random.random() * 2)
+
+def get_novel_id_DB(novel_name,novel_author):
+    novel_name=novel_name.strip()
+    novel_author=novel_author.strip()
+    conn = MySQLdb.connect(user='root', passwd='123456',
+                           host='localhost', db='langdinovel', charset='utf8')
+    cur = conn.cursor()
+    sql = "select id,novel_catalog_url,novel_name,novel_author from novels WHERE novel_name=%s and novel_author=%s"
+    bol = cur.execute(sql, (novel_name,novel_author))
+    if(1==bol):
+        novel = cur.fetchone()
+        return novel
+    else:
+        return 'nonono'
+
+def get_chapter_text(url,novel_info,index):
+    try:
+        while (True):
+            time.sleep(random.random() * 2)
+            print 'working>>>', novel_info[2], novel_info[3],index, 'cap:', 'url:', url
+            req_header = getHeader()
+            req = urllib2.Request(url, None, req_header)
+            resp = urllib2.urlopen(req, None)
+            html = resp.read()
+            if(not resp.getcode()==200)
+                print 'time out！ repeat', url
+                continue
+            soup = BeautifulSoup(html, 'html.parser', from_encoding='gbk')
+            try:
+                ispage=soup.find('title').get_text()
+                if(novel_info[2] in ispage):
+                    break
+            except:
+                print 'repeat',url
+        content = soup.find('dd', id="contents")
+        title = soup.find('h1')
+        put_text_DB(novel_info[0], title.get_text(), content.get_text())
+        next_page = soup.find_all('a', href=re.compile("[0-9]+.html$"))
+        for next in next_page:
+            if (next.get_text() in '下一页'):
+                return next['href']
+    except:
+        print 'error cap:',url
+        import traceback
+        traceback.print_exc()
+        exit(-1)
+
+def put_text_DB(novel_id,catalog_name,catalog_content):
+    conn = MySQLdb.connect(user='root', passwd='123456',
+                           host='localhost', db='langdinovel', charset='utf8')
+    cur = conn.cursor()
+    sql = "insert into catalogs(novel_id,catalog_name,catalog_content) VALUES (%s,%s,%s)"
+    bol = cur.execute(sql, (novel_id,catalog_name,catalog_content))
+    if(bol==0):
+        print 'DB error! exit'
+        exit(-1)
+    cur.close()
+    conn.commit()
+    conn.close()
+host='http://www.23wx.com'
+start_read('儒道至圣','永恒之火')
